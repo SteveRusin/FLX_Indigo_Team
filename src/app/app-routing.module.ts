566a4263a32a903cc176ad5854e3d0c784a4dc8b
Denys Samotos <samotos.dan@gmail.com>
2019-04-12 00:00:24 +0300

    added routerLink to profile ref

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
-export const routes = [
-  {path: 'profile', loadChildren: './player-profile/player-profile.module#PlayerProfileModule' }
+export const routes: {
+  path: string; loadChildren: string; }[] = [
+  {path: 'profile', loadChildren: './player-profile/player-profile.module#PlayerProfileModule' },
 ];
