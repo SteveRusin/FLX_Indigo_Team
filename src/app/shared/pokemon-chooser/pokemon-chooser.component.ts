850eb99cfe1117a89919a8e2116ef588871de21c
MelnikovRuslan <melnikovruslan23@gmail.com>
2019-04-23 15:17:25 +0300

    updated chooser

--- src/app/shared/pokemon-chooser/pokemon-chooser.component.ts
+++ src/app/shared/pokemon-chooser/pokemon-chooser.component.ts
@@ -1,69 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 import { MatStepper } from '@angular/material/stepper';
 import { CdkStep } from '@angular/cdk/stepper';
+import { PokemonInterface } from './pokemon-interface';
+import { pokemons } from './pokemon-list';
+
 @Component({
   selector: 'app-pokemon-chooser',
   templateUrl: './pokemon-chooser.component.html',
   styleUrls: ['./pokemon-chooser.component.scss']
 })
+
 export class PokemonChooserComponent implements OnInit {
 
-  constructor() { }
-  public pokemons: any = [
-    {
-      id: 1,
-      name: 'Bulbasaur',
-      type: [ 'grass', 'poison' ],
-      sprite: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png'
-    },
-    {
-      id: 5,
-      name: 'Charmeleon',
-      type: [ 'fire' ],
-      sprite: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/5.png'
-    },
-    {
-      id: 15,
-      name: 'Beedrill',
-      type: [ 'bug', 'poison' ],
-      sprite: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/15.png'
-    },
-    {
-      id: 193,
-      name: 'Yanma',
-      type: [ 'bug', 'flying' ],
-      sprite: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/193.png'
-    },
-    {
-      id: 152,
-      name: 'Chikorita',
-      type: [ 'grass' ],
-      sprite: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/152.png'
+  constructor() {}
+
+  public pokemonsList: PokemonInterface[] = pokemons;
+  public selectedPokemon: PokemonInterface[] = [];
+
+  public choosePokemon(pokemon: PokemonInterface, step: CdkStep, stepper: MatStepper): void {
+    if(stepper.selectedIndex === 0) {
+      this.selectedPokemon[0] = pokemon;
+    } else {
+      this.selectedPokemon[1] = pokemon;
     }
-    ];
-
-    public pokemonOne: any = this.pokemons[0];
-    public pokemonTwo: any = this.pokemons[0];
-
-    public choosePokemon(id: number, step: CdkStep, stepper: MatStepper): void {
-      if(stepper.selectedIndex === 0) {
-        this.pokemonOne = this.getPokemon(id);
-      } else {
-        this.pokemonTwo = this.getPokemon(id);
-      }
-      if(!step.completed) {
-        step.completed = true;
-      }
-      stepper.next();
+    if(!step.completed) {
+      step.completed = true;
     }
+    stepper.next();
+  }
 
-    public getPokemon(id: number): any {
-      return this.pokemons.find((pokemon: any) => pokemon.id === id);
-    }
-
-    public fightMessage(): void {
-      alert('FIGHT');
-    }
-
-    public ngOnInit(): void {}
+  public ngOnInit(): void {}
 }
--- src/app/shared/pokemon-chooser/pokemon-chooser.module.ts
+++ src/app/shared/pokemon-chooser/pokemon-chooser.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { MatStepperModule } from '@angular/material/stepper';
 import { MatIconModule } from '@angular/material/icon';
 import { PokemonChooserComponent } from './pokemon-chooser.component';
+import { PokemonCardComponent } from './pokemon-card/pokemon-card.component';
 
 @NgModule({
-  declarations: [PokemonChooserComponent],
+  declarations: [PokemonChooserComponent, PokemonCardComponent],
   imports: [
     CommonModule,
     MatStepperModule,
