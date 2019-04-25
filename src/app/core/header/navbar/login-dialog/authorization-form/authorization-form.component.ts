fa1e9f7cdf2a5a0a94e09baf549b0318b3384e41
Denys Samotos <samotos.dan@gmail.com>
2019-04-26 00:52:51 +0300

    added authorization

---
+++ src/app/core/header/navbar/login-dialog/authorization-form/authorization-form.component.ts
@@ -0,0 +1,51 @@
+import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { MatDialogRef } from '@angular/material';
+
+import { AuthService } from 'src/app/services/auth.service';
+import { LoginDialogComponent } from '../login-dialog.component';
+
+@Component({
+  selector: 'dialog-authorization-form',
+  templateUrl: './authorization-form.component.html',
+  styleUrls: ['./authorization-form.component.scss']
+})
+export class AuthorizationFormComponent {
+  public hide: boolean = true;
+  public nickname: string;
+  public email: FormControl = new FormControl('', [Validators.required, Validators.email]);
+  public password: string;
+  public authorizationForm: FormGroup;
+
+  constructor(public dialogRef: MatDialogRef<LoginDialogComponent>,
+    public authService: AuthService) {
+      this.authorizationForm = new FormGroup({
+        nickname: new FormControl(),
+        email: new FormControl(),
+        password: new FormControl()
+      });
+  }
+
+  public getErrorMessage(): string {
+    return this.email.hasError('required') ? 'Please enter a valid email address' :
+           this.email.hasError('email') ? 'Not a valid email' : '';
+  }
+
+  public onSubmit(): any {
+    // TODO: Use EventEmitter with form value
+    console.warn(this.email.value);
+  }
+
+  public authorizationWithEmailAndPassword(): void {
+    this.authService
+      .createUserWithEmailAndPassword(this.authorizationForm.value.email,
+                                      this.authorizationForm.value.password,
+                                      this.authorizationForm.value.nickname);
+    this.dialogRef.close();
+  }
+
+  public authorizationWithGoogle(): void {
+    this.authService.signInWithGoogle();
+    this.dialogRef.close();
+  }
+}
---
+++ src/app/core/header/navbar/login-dialog/login-form/login-form.component.ts
@@ -0,0 +1,54 @@
+import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { MatDialogRef } from '@angular/material';
+
+import { AuthService } from 'src/app/services/auth.service';
+import { LoginDialogComponent } from '../login-dialog.component';
+import { LoginDialogService } from './../login-dialog.service';
+
+@Component({
+  selector: 'dialog-login-form',
+  templateUrl: './login-form.component.html',
+  styleUrls: ['./login-form.component.scss']
+})
+
+export class LoginFormComponent {
+  public hide: boolean = true;
+  public email: FormControl = new FormControl('', [Validators.required, Validators.email]);
+  public password: string;
+  public loginForm: FormGroup;
+
+  constructor(public dialogRef: MatDialogRef<LoginDialogComponent>,
+    public loginDialogService: LoginDialogService, public authService: AuthService) {
+      this.loginForm = new FormGroup({
+        email: new FormControl(),
+        password: new FormControl()
+      });
+  }
+
+  public signInForm(): boolean {
+    return this.loginDialogService.signInForm();
+  }
+
+  public getErrorMessage(): string {
+    return this.email.hasError('required') ? 'Please enter a valid email address' :
+           this.email.hasError('email') ? 'Not a valid email' : '';
+    }
+
+  public onSubmit(): any {
+    // TODO: Use EventEmitter with form value
+    console.warn(this.email.value);
+  }
+
+  public signInWithGoogle(): void {
+    this.authService.signInWithGoogle();
+    this.dialogRef.close();
+  }
+
+  public signInWithEmailAndPassword(): void {
+    console.log(this.loginForm.value);
+    this.authService
+      .signInWithEmailAndPassword(this.loginForm.value.email, this.loginForm.value.password);
+    this.dialogRef.close();
+  }
+}
