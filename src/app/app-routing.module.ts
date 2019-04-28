ffd2288f01fee801f3638d0452eea5679f92b5de
Denys Samotos <samotos.dan@gmail.com>
2019-04-28 16:23:56 +0300

    added route guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,17 @@
-import { Routes } from '@angular/router';
+import { Routes, CanActivate  } from '@angular/router';
+import {
+  AuthGuardService as AuthGuard
+} from './services/auth-guard.service';
 
 export const routes: Routes = [
-  { path: 'profile', loadChildren: './player-profile/player-profile.module#PlayerProfileModule' },
-  { path: '', loadChildren: './home-page/home-page.module#HomePageModule'}
+  {
+    path: '',
+    loadChildren: './home-page/home-page.module#HomePageModule',
+  },
+  {
+    path: 'profile',
+    loadChildren: './player-profile/player-profile.module#PlayerProfileModule',
+    canActivate: [AuthGuard]
+  },
+  { path: '**', redirectTo: '' }
 ];
